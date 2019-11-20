2e0dbf41c11e538add69388ccc3b07228e862565
Tolga Kurtuluş <43134032+tolgakurtulus@users.noreply.github.com>
2019-11-20 12:51:59 +0300

    Add files via upload

---
+++ MainContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, neque nesciunt commodi nostrum officia dolores facere earum aspernatur veritatis quisquam distinctio id sapiente eaque dolore ipsa adipisci ullam pariatur quidem!</p>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+
+    return{
+      text : "Lorem ipsum"
+
+
+    }
+
+
+
+  }
+  
+}
+</script>
+
+<style>
+  
+</style>
---
+++ Product.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="product">
+
+    <strong>{{ product.title }}</strong>
+    <p>{{ product.description }}</p>
+    <strong>Price : {{ product.price }}| Pcs : {{ product.count }}</strong>
+    <br><br>
+    <button @click="addToCart(product)">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props : ["product"],
+  methods: {
+    addToCart(product){
+      this.$emit("addToCart", product)
+    }
+  },
+  
+}
+</script>
+
+<style>
+
+.product{
+
+  border: 2px dashed blueviolet;
+  padding: 5px 10px;
+  width: 300px;
+  height: 125px;
+  margin-bottom: 30px;
+
+}
+
+.product:hover{
+
+  border: 2px solid;
+  transition: all 1s;
+}
+  
+</style>
